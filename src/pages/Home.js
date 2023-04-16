7c66988b2a2a66e045a8e1656a497f4b2ea89d97
CkhGueye <alabaygueye@gmail.com>
2023-04-16 15:03:25 +0000

    randomize home videos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 import { VideosGrid } from "../components";
 import { customFetch } from "../utils/customFetch";
@@ -8,12 +8,29 @@ const Home = () => {
   const { category } = useParams();
   const [videos, setVideos] = useState(null);
 
+  const getRandomVideos = useCallback(async () => {
+    const videos = await customFetch(
+      `search?part=snippet&q=new%20technology`
+    ).then((data) => data.items);
+
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    const randomVideoId = videos[randomIndex].id.videoId;
+
+    customFetch(
+      `search?part=snippet&relatedToVideoId=${randomVideoId}&type=video`
+    ).then((data) => setVideos(data.items));
+  }, []);
+
   useEffect(() => {
     setVideos(null);
-    customFetch(
-      `search?part=snippet&q=${category === "home" ? "" : category}`
-    ).then((data) => setVideos(data.items));
-  }, [category]);
+    if (!category) {
+      getRandomVideos();
+    } else {
+      customFetch(`search?part=snippet&q=${category}`).then((data) =>
+        setVideos(data.items)
+      );
+    }
+  }, [category, getRandomVideos]);
 
   return (
     <>
